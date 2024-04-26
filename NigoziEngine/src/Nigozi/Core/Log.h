7df9c222f0d089cde47d252f1d1133fc80d7bf1e
MustachoMan <fguru974@gmail.com>
2024-04-26 20:51:58 +0400

    Slight changes to Logging MACROS

--- NigoziEngine/src/Nigozi/Core/Log.h
+++ NigoziEngine/src/Nigozi/Core/Log.h
@@ -1,20 +1,27 @@
 #pragma once
 #include "ngpch.h"
+#include <windows.h>
+
 
 #ifdef _DEBUG
+
+	inline HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	// Check if a task is succesfully completed
 	#define ASSERT(condition, message) \
 			if ((condition)){ \
+				SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN); \
 				std::cerr << message " -> Success!" << std::endl; \
 			} \
 			else { \
+				SetConsoleTextAttribute(hConsole, FOREGROUND_RED); \
 				std::cerr << message << " -> Failed!" << std::endl; \
 				__debugbreak(); \
-			}
+			} \
+			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 	#define LOG(message) \
 				std::cout << message << std::endl;
 #else
-	#define ASSERT(condition, message) ((void)0)
-	#define LOG(message) ((void)0)
+	#define ASSERT(condition, message)
+	#define LOG(message)
 #endif
