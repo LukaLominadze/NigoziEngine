3a84298f97ff663fa58f96b9dbeb0ec1355ad8db
MustachoMan <fguru974@gmail.com>
2024-04-28 23:50:57 +0400

    Updated Input Class

--- NigoziEngine/src/Nigozi/Events/Input.h
+++ NigoziEngine/src/Nigozi/Events/Input.h
@@ -11,16 +11,12 @@ namespace Nigozi
 		Input(EventHandler* eventHandler);
 		~Input();
 
-		static bool KeyPressed(SDL_Scancode keycode) { return s_instance->KeyPressedImpl(keycode); }
-		static bool KeyDown(SDL_Scancode keycode) { return s_instance->KeyDownImpl(keycode); }
-		static bool KeyReleased(SDL_Scancode keycode) { return s_instance->KeyReleasedImpl(keycode); }
+		static bool KeyPressed(SDL_Scancode keycode) { return s_instance->p_eventHandler->KeyPressed(keycode); }
+		static bool KeyDown(SDL_Scancode keycode) { return s_instance->p_eventHandler->KeyDown(keycode); }
+		static bool KeyReleased(SDL_Scancode keycode) { return s_instance->p_eventHandler->KeyReleased(keycode); }
 
 		inline static Input& Get() { return *s_instance; }
 	private:
-		virtual bool KeyPressedImpl(SDL_Scancode keycode) { return p_eventHandler->KeyPressed(keycode); }
-		virtual bool KeyDownImpl(SDL_Scancode keycode) { return p_eventHandler->KeyDown(keycode); }
-		virtual bool KeyReleasedImpl(SDL_Scancode keycode) { return p_eventHandler->KeyReleased(keycode); }
-
 		static Input* s_instance;
 
 		EventHandler* p_eventHandler;
