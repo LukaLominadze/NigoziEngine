f760baa60bf15256c4e6492bed1168e1a935d82e
LukaLominadze <fguru74@gmail.ccom>
2026-04-16 00:05:24 +0400

    minor refactors in c# input class

--- Projects/First/Assets/Scripts/Player.cs
+++ Projects/First/Assets/Scripts/Player.cs
@@ -21,10 +21,10 @@ namespace First
 
         public void OnUpdate(float timestep)
         {
-            float horizontalAxis = Input.GetAxis(Input.Keys.A, Input.Keys.D);
+            float horizontalAxis = Input.GetAxis(Keys.A, Keys.D);
             Position += new Vector2(horizontalAxis * 5.0f * timestep, 0.0f);
 
-            if (Input.IsKeyJustPressed(Input.Keys.Space))
+            if (Input.IsKeyJustPressed(Keys.Space))
             {
                 ApplyImpulse(Vector2.Up * 8.0f);
             }
